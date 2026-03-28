5a05af5305c90d97d187c3909dfb18af25618702
Asma <asmabentaherr@gmail.com>
2026-03-28 16:23:50 +0200

    Switch to webhook #3

--- bot/main.py
+++ bot/main.py
@@ -37,7 +37,7 @@ def home():
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
     json_str = request.get_data().decode("UTF-8")
-    update = bot.types.Update.de_json(json_str)
+    update = telebot.types.Update.de_json(json_str)
     bot.process_new_updates([update])
     return "OK", 200
 
