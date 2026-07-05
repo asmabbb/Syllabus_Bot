44cadab665e89413e881a270761ae12392934836
Asma <asmabentaherr@gmail.com>
2026-07-05 17:46:17 +0200

    Fix Bug #1

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,7 +1,7 @@
 from bot.bot_instance import bot
 from bot.database.queries.users import get_user, create_user
 from bot.keyboards.main_menu_keyboard import main_menu
-from bot.config import is_admin, is_super_admin
+from bot.utils.permissions import is_admin, is_super_admin
 from bot.handlers.admin_panel import admin_state
 
 
